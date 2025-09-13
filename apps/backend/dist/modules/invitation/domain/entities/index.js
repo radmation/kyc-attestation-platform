cb2b38f47a97e5e90fec725536ff20ffee184687
radmation <radmation@yahoo.com>
2025-09-13 12:00:06 -0700

    Remove dist folders from git tracking and update .gitignore

--- apps/backend/dist/modules/invitation/domain/entities/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserInvitation = void 0;
-var user_invitation_entity_1 = require("./user-invitation.entity");
-Object.defineProperty(exports, "UserInvitation", { enumerable: true, get: function () { return user_invitation_entity_1.UserInvitation; } });
-//# sourceMappingURL=index.js.map
